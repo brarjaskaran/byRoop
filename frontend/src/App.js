4cef032cf40b02aa4024d2b416ac3c295585c55b
Jaskaran Brar <jaskaranbrar77@gmail.com>
2020-10-19 13:57:12 +1030

    serving and fetching data from express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
-  return <>
-  <h1>welcome
-    
-  </h1>
-  </>;
+  return (
+    <Router>
+    <>
+      <Header/>
+        <main className="py-3">
+          <Container>
+            <h1>welcome to byroop </h1>
+            <Route path="/" component={HomeScreen} exact/>
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </main>
+      <Footer/>
+    </>
+    </Router>
+  )
 }
 
-export default App;
+export default App
